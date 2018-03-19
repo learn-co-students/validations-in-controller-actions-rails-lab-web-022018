e241887d8d41ea22cea40f6ce02e8d16ef155d35
vsparrow <khan@vsparrow.com>
2018-03-19 17:52:25 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ActiveRecord::Base
+#   Add validations to Author such that...
+# The name cannot be blank
+ validates :name, presence: true
+# validates :name, uniqueness: true #, uniqueness: true
+# The e-mail is unique
+validates :email, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+#
+#   he title cannot be blank
+ validates :title, presence: true
+
+# The category is either "Fiction" or "Non-Fiction"
+  validates :category, inclusion: %w(Fiction Non-Fiction) #  validates :category, inclusion: %w(Fiction Non-Fiction)#%w(male female)
+
+# The content is at least 100 characters long.
+  validates :content, length: {minimum: 100}
 end
